ee5bfa4a0101694a512c6c60a97845392f83f93d
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2016-08-29 11:22:36 -0700

    Removing irrelevant comments

--- tests/lists.js
+++ tests/lists.js
@@ -13,10 +13,6 @@ for (var i = 5; i >= 1; i--) {
 function case1() {
 	node = list;
 	return node.next.next.next.data;
-	// Trace: (f1, i1) (f1, i2) (f1, i3)
-	// eidx1 = [(i1, 1)]       $ i1
-	// eidx2 = [(i2, 1)]       $ i2
-	// eidx3 = [(i3, 1)]       $ i3
 }
 
 // Case 2: Traversal
@@ -26,10 +22,6 @@ function case2() {
 		node = node.next;
 	}
 	return node.data;
-	// Trace: (f1, i1) (f1, i1) (f1, i1)
-	// eidx1 = [(i1, 1)]        $ i1
-	// eidx2 = [(i1, 2)]        $ i1
-	// eidx3 = [(i1, 3)]        $ i1
 }
 
 function contains(node, x) {
@@ -44,11 +36,6 @@ function contains(node, x) {
 // Case 3: Traversal
 function case3() {
 	return contains(list, 42);
-	// Trace: (f2, i1) (f3, i1) (f4, i1)
-		// where f2 = f1.c1, f3 = f2.c2, f4 = f3.c2
-	// eidx1 = [(c1, 1), (i1, 1)]                    $ c1, i1
-	// eidx2 = [(c1, 1), (c2, 1), (i1, 1)]           $ c1, c2, i1
-	// eidx3 = [(c1, 1), (c2, 1), (c2, 1), (i1, 1)]  $ c1, c2, i1
 }
 
 // Case 4: Not a traversal
@@ -56,15 +43,7 @@ function case4(){
 	function next(node) {
 		return node.next;
 	}
-	return next(
-		next(
-			next(
-				list))).data;
-	// Trace: (f2, i1) (f3, i1) (f4, i1)
-		// where f2 = f1.c1, f3 = f1.c2, f4 = f1.c3
-	// eidx1 = [(c1, 1), (i1, 1)]  $ c1, i1
-	// eidx2 = [(c2, 1), (i1, 1)]  $ c2, i1
-	// eidx3 = [(c3, 1), (i1, 1)]  $ c3, i1
+	return next(next(next(list))).data;
 }
 
 // Case 5: Traversal
@@ -76,11 +55,6 @@ function case5() {
 	while (node.next != null) {
 		node = next(node);
 	}
-	// Trace: (f2, i1) (f3, i1) (f4, i1)
-	   // where f2 = f1.c1, f3 = f1.c1, f4 = f1.c1
-	// eidx1 = [(c1, 1) (i1, 1)]                   $ c1, i1
-	// eidx2 = [(c1, 2) (i1, 1)]                   $ c1, i1
-	// eidx3 = [(c1, 3) (i1, 1)]                   $ c1, i1
 }
 
 
@@ -97,11 +71,6 @@ function case6() {
 	while (node != null) {
 		node = next(node);
 	}
-	// Trace: (f3, i1) (f5, i1) (f7, i1)
-	   // where f2 = f1.c1, f3 = f2.c2, f4 = f1.c1, f5 = f4.c2, f6 = f1.c1, f7 = f6.c2
-	// eidx1 = [(c1, 1), (c2, 1), (i1, 1)]        $ c1, c2, i1
-	// eidx2 = [(c1, 2), (c2, 1), (i1, 1)]        $ c1, c2, i1
-	// eidx3 = [(c1, 3), (c2, 1), (i1, 1)]        $ c1, c2, i1
 }
 
 // Case 7: Traversal
@@ -128,6 +97,7 @@ function repeat(f, n) {
 	}());
 }
 
+// Some redundant traversals
 repeat(case6, 3);
 repeat(case7, 3);
 repeat(case7, 3);
