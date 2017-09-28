038d2a760bce4e9d92ec6af54f2fec38d6681dbe
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2017-09-27 20:53:25 -0700

    Allow configuring NodeJS and Python runnables from environment vars

--- bin/collect_trace.sh
+++ bin/collect_trace.sh
@@ -4,7 +4,9 @@ TRAVIOLI_DIR="$(dirname $0)/.."
 JALANGI_DIR="$1"
 
 # Set JS runtime
-node="node"
+if [ -z "$NODEJS" ]; then
+  export NODEJS="node"
+fi
 
 if [ $# -lt 2 ]; then
   echo "Usage: $0 JALANGI_DIR JS_FILE [ARGS]" >&2
@@ -12,4 +14,4 @@ if [ $# -lt 2 ]; then
 fi
 
 mkdir -p ".travioli"
-node "$JALANGI_DIR/src/js/commands/jalangi.js" --inlineIID --inlineSource --analysis "$JALANGI_DIR/src/js/sample_analyses/ChainedAnalyses.js" --analysis "$JALANGI_DIR/src/js/runtime/SMemory.js" --analysis "$TRAVIOLI_DIR/src/js/LogData.js" "${@:2}"
+"$NODEJS" "$JALANGI_DIR/src/js/commands/jalangi.js" --inlineIID --inlineSource --analysis "$JALANGI_DIR/src/js/sample_analyses/ChainedAnalyses.js" --analysis "$JALANGI_DIR/src/js/runtime/SMemory.js" --analysis "$TRAVIOLI_DIR/src/js/LogData.js" "${@:2}"
--- bin/run.sh
+++ bin/run.sh
@@ -3,17 +3,21 @@
 TRAVIOLI_DIR="$(dirname $0)/.."
 
 # Set python runtime
-if which pypy > /dev/null; then
-	PYTHON="pypy"
-else
-	PYTHON="python"
+if [ -z "$PYTHON" ]; then
+  if which pypy > /dev/null; then
+    export PYTHON="pypy"
+  else
+    export PYTHON="python"
+  fi
 fi
 
 # Set JS runtime
-NODE="node"
+if [ -z "$NODEJS" ]; then
+  export NODEJS="node"
+fi
 
-$NODE $@ &&
+"$NODEJS" $@ &&
 "$TRAVIOLI_DIR/bin/collect_trace.sh" "$TRAVIOLI_DIR/node_modules/jalangi2" $@ &&
-$PYTHON "$TRAVIOLI_DIR/src/py/readtrace.py" && 
+"$PYTHON" "$TRAVIOLI_DIR/src/py/readtrace.py" && 
 tail -3 ".travioli/traversals.out"
 
